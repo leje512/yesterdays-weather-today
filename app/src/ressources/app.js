8c81e749d5a19a92bd5b5b7b26036b3de6228c37
jelllea <lea.jell@hs-augsburg.de>
2021-12-10 14:04:16 +0100

    Update dateFormat to only show date no time

--- app/src/ressources/app.js
+++ app/src/ressources/app.js
@@ -2,7 +2,7 @@ import moment from "moment";
 
 function formatDate(value) {
   if (value) {
-    return moment(String(value)).format("MM/DD/YYYY hh:mm");
+    return moment(String(value)).format("MM/DD/YYYY");
   }
 }
 
