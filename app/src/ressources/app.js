4ff2d3ecea6661ab7fd19c998c7b8b3d91c1b37b
jelllea <lea.jell@hs-augsburg.de>
2021-12-10 14:59:46 +0100

    Add method to calculate yesterdaysdate with a format conforming to api call

--- app/src/ressources/app.js
+++ app/src/ressources/app.js
@@ -1,8 +1,19 @@
 import moment from "moment";
 
-function formatDate(value) {
+export function formatDate(value) {
   if (value) {
-    return moment(String(value)).format("MM/DD/YYYY");
+    return moment(String(value)).format("DD/MM/YYYY");
+  }
+}
+
+export function yesterdaysDate(value) {
+  if (value) {
+    console.log(value);
+    let day = value.slice(0, 2) - 1;
+    let rest = value.slice(2, value.length + 1);
+    let yesterday = day + rest;
+    console.log(moment(String(yesterday)).format("YYYY-DD-MM"));
+    return moment(String(yesterday)).format("YYYY-DD-MM");
   }
 }
 
