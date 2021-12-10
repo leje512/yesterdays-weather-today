0fbf195670ae17835469edaa7797506d108f3add
jelllea <lea.jell@hs-augsburg.de>
2021-12-10 14:59:05 +0100

    Add axios call to yesterdays weather and add export

--- app/src/services/rest.js
+++ app/src/services/rest.js
@@ -5,7 +5,7 @@ const key = "19a6a6146da04aae80e131758210212";
 const latitude = "48.342852";
 const longtitude = "10.905494";
 
-async function getCurrentWeather() {
+export async function getCurrentWeather() {
   const response = axios.get(
     `${baseUrl}/forecast.json?key=${key}&q=${latitude},${longtitude}&days=1`
   );
@@ -13,4 +13,12 @@ async function getCurrentWeather() {
   return data;
 }
 
-export default getCurrentWeather;
+export async function getYesterdaysWeather(date) {
+  const response = axios.get(
+    `${baseUrl}/history.json?key=${key}&q=${latitude},${longtitude}&dt=${date}`
+  );
+  const data = (await response).data;
+  return data;
+}
+
+export default { getCurrentWeather, getYesterdaysWeather };
