f4e2c98a760f8d0968eb02a0425737aa1a5cfd11
jelllea <lea.jell@hs-augsburg.de>
2021-12-10 15:52:06 +0100

    Edit fontsize

--- app/src/App.vue
+++ app/src/App.vue
@@ -19,6 +19,7 @@ export default {
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-size: 18px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
